e342b509fbf6c76fffd31eb24e9a488b7862559e
Admin <heinerbauza@gmail.com>
2019-07-23 18:02:31 -0400

    finish happy birthday

--- app/src/main/java/com/mobileapps/helloworld/MainActivity.kt
+++ app/src/main/java/com/mobileapps/helloworld/MainActivity.kt
@@ -2,11 +2,19 @@ package com.mobileapps.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClick(view : View)
+    {
+        val name : String = tvName.text.toString() + " " + etEnterName.text
+        tvName.text = name
     }
 }
